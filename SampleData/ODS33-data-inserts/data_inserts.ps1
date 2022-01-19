879fce6c9dbe83ec59260ef47ba28b139c3bafaf
NSD-Daniel Rosado <daniel.rosado@nearshoredevs.com>
2022-01-19 10:23:05 -0600

    BIA-945: Changing script to allow instance name and database name modification

--- SampleData/ODS33-data-inserts/data_inserts.ps1
+++ SampleData/ODS33-data-inserts/data_inserts.ps1
@@ -3,14 +3,15 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
+$ServerInstance = 'localhost'
+$DatabaseName = 'EdFi_Ods'
 
-
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\learningStandards.sql"
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\enableStaffSectionAssociation.sql"
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\data.sql"
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\data_Assessments_1.sql"
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\data_Assessments_2.sql"
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\data_Assessments_3.sql"
-Invoke-Sqlcmd -ServerInstance ".\SQLEXPRESS" -Database "Edfi_Ods" -InputFile ".\data_Assessments_4.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\learningStandards.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\enableStaffSectionAssociation.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\data.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\data_Assessments_1.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\data_Assessments_2.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\data_Assessments_3.sql"
+Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $DatabaseName -InputFile ".\data_Assessments_4.sql"
 
 
