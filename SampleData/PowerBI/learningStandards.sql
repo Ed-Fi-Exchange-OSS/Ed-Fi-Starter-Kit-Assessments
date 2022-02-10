ab05f6289c09d0cfd04647c440daa3585933c745
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-02-10 15:17:37 -0600

    [BIA-982] Removes the database from the sample data insert scripts. Adds the index.html  (#7)
    * Removes the database from the sample data insert scripts.
    
    * Adds the index.html file.

--- SampleData/PowerBI/learningStandards.sql
+++ SampleData/PowerBI/learningStandards.sql
@@ -3,14 +3,14 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-INSERT INTO EdFi_Ods.edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
+INSERT INTO edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
     VALUES ('224.48.NA.9.3.A',
         'compare and order rational numbers in various forms including integers, percents, and positive and negative fractions and decimals;',
         'Reading, Grade 5',
         'uri://ed-fi.org/Assessment/Assessment.xml',
         '8319383D-9276-4FD2-AB70-E23BB6493200')
         
-INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
+INSERT INTO edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
     VALUES ('AMT-2011-ARTS-Fifth grade',
         'State Assessment_Reading5-1',
         '224.48.NA.9.3.A',
@@ -18,14 +18,14 @@ INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentif
 
 ---
 
-INSERT INTO EdFi_Ods.edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
+INSERT INTO edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
     VALUES ('346.44.8.5.A',
         'use place value to read, write, compare, and order whole numbers through the 999,999,999,999.',
         'Mathematics, Grade 5',
         'uri://ed-fi.org/Assessment/Assessment.xml',
         '8319383D-9276-4FD2-AB70-E23BB6493210')
 
-INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
+INSERT INTO edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
     VALUES ('AMT-2011-Math-Fifth grade',
         'State Assessment_Math5-1',
         '346.44.8.5.A',
@@ -33,14 +33,14 @@ INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentif
 
 ---
 
-INSERT INTO EdFi_Ods.edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
+INSERT INTO edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
     VALUES ('449.41.10.A',
         'adjust fluency when reading aloud grade-level text based on the reading purpose and the nature of the text.',
         'English Language Arts and Reading, Grade 8, 2009-2010',
         'uri://ed-fi.org/Assessment/Assessment.xml',
         '8319383D-9276-4FD2-AB70-E23BB6493230')
 
-INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
+INSERT INTO edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
     VALUES ('AMT-2011-Science-Fifth grade',
         'State Assessment_Science5-1',
         '449.41.10.A',
@@ -48,14 +48,14 @@ INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentif
 
 ---
 
-INSERT INTO EdFi_Ods.edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
+INSERT INTO edfi.LearningStandard (LearningStandardId,Description,CourseTitle,Namespace,Id)
     VALUES ('995.48.5.A',
         'read aloud grade-level stories with fluency (rate, accuracy, expression, appropriate phrasing) and comprehension.',
         'English Language Arts and Reading, Grade 5, 2009-2010',
         'uri://ed-fi.org/Assessment/Assessment.xml',
         '8319383D-9276-4FD2-AB70-E23BB6493240')
 
-INSERT INTO EdFi_Ods.edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
+INSERT INTO edfi.ObjectiveAssessmentLearningStandard (AssessmentIdentifier,IdentificationCode,LearningStandardId,Namespace)
     VALUES ('AMT-2011-Writing-Fifth grade',
         'State Assessment_Writing5-1',
         '995.48.5.A',
